abd8781df7b201c1babc31a23a95fbe61b73ef0b
swasta <link.bor@gmail.com>
2015-11-01 00:48:11 +0300

    Fetching improvements

---
+++ src/main/java/Main/Model/Book.java
@@ -0,0 +1,9 @@
+package Main.Model;
+
+/**
+ * Created by nikitaborodulin on 01/11/15.
+ */
+public class Book extends Publication {
+    String publisher;
+    String ISBN;
+}
--- src/main/java/Main/sql2omodel/Sql2oModel.java
+++ src/main/java/Main/sql2omodel/Sql2oModel.java
@@ -4,12 +4,14 @@ package Main.sql2omodel;
 //import me.tomassetti.RandomUuidGenerator;
 //import me.tomassetti.UuidGenerator;
 
+import Main.Model.Article;
 import Main.Model.Model;
 import Main.Model.Participant;
 import Main.Model.Publication;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sql2oModel implements Model {
@@ -93,6 +95,48 @@ public class Sql2oModel implements Model {
         }
     }*/
 
+    public List<Publication> getBooksOn(String attribute, String value) {
+        String sql;
+        if (attribute.equals("Publisher")) {
+            sql = "SELECT pub_id, title, year, book.url, publisher, isbn " +
+                    "FROM book JOIN publication ON book.key = publication.pub_key " +
+                    "WHERE publisher = :target " +
+                    "limit 10";
+        } else {
+            sql = "SELECT pub_id, title, year, book.url, publisher, isbn " +
+                    "FROM book JOIN publication ON book.key = publication.pub_key " +
+                    "WHERE month = :target " +
+                    "limit 10";
+        }
+        try (Connection conn = sql2o.open()) {
+            List<Main.Model.Book> books = conn.createQuery(sql)
+                    .addParameter("target", value)
+                    .executeAndFetch(Main.Model.Book.class);
+            return new ArrayList<>(books);
+        }
+    }
+
+    public List<Publication> getArticlesOn(String attribute, String value) {
+        String sql;
+        if (attribute.equals("Journal")) {
+            sql = "SELECT pub_id, title, year, article.url, journal, month, volume, number " +
+                    "FROM article JOIN publication ON article.key = publication.pub_key " +
+                    "WHERE journal = :target " +
+                    "limit 10";
+        } else {
+            sql = "SELECT pub_id, title, year, article.url, journal, month, volume, number " +
+                    "FROM article JOIN publication ON article.key = publication.pub_key " +
+                    "WHERE month = :target " +
+                    "limit 10";
+        }
+        try (Connection conn = sql2o.open()) {
+            List<Article> articles = conn.createQuery(sql)
+                    .addParameter("target", value)
+                    .executeAndFetch(Article.class);
+            return new ArrayList<>(articles);
+        }
+    }
+
     @Override
     public List<Publication> getPublicationsOn(String attribute, String value) {
         String sql;
@@ -109,43 +153,22 @@ public class Sql2oModel implements Model {
                         "FROM  publication " +
                         "WHERE year = :target " +
                         "limit 10";
-
                 break;
             case "Journal":
-                sql = "SELECT pub_id, title, year, url " +
-                        "FROM  publication " +
-                        "WHERE journal = :target " +
-                        "limit 10";
-
-                break;
+                return getArticlesOn(attribute, value);
             case "Month":
-                sql = "SELECT pub_id, title, year, url " +
-                        "FROM  publication " +
-                        "WHERE month = :target " +
-                        "limit 10";
-
-                break;
+                return getArticlesOn(attribute, value);
             case "Publisher":
-                sql = "SELECT pub_id, title, year, url " +
-                        "FROM  publication " +
-                        "WHERE publisher = :target " +
-                        "limit 10";
-
-                break;
+                return getBooksOn(attribute, value);
             case "ISBN":
-                sql = "SELECT pub_id, title, year, url " +
-                        "FROM  publication " +
-                        "WHERE ISBN = :target " +
-                        "limit 10";
-
-                break;
+                return getBooksOn(attribute, value);
             default:
                 sql = "null";
                 break;
         }
         try (Connection conn = sql2o.open()) {
             List<Publication> publications = conn.createQuery(sql)
-                    .addParameter("target",value)
+                    .addParameter("target", value)
                     .executeAndFetch(Publication.class);
             return publications;
         }
@@ -161,7 +184,7 @@ public class Sql2oModel implements Model {
         try (Connection conn = sql2o.open()) {
             if (attribute.equals("1")) {
                 List<Participant> participants = conn.createQuery(sql)
-                        .addParameter("targetName",value)
+                        .addParameter("targetName", value)
                         .executeAndFetch(Participant.class);
                 return participants;
             } else
